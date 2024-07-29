dd23aa0a5109b0522136a760166fda216b516a17
alifatihuzun <109788254+alifatihuzun@users.noreply.github.com>
2024-07-29 11:53:14 +0300

    RESTful api in aspnet(pratice for get etc methods)

---
+++ RESTful api in aspnet/Controllers/BankSystemController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTful_api_in_aspnet.Models;
+
+namespace RESTful_api_in_aspnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankSystemController : ControllerBase
+    {
+        public static List<BankSystem> listofBankSystems = new List<BankSystem>
+        {
+            new BankSystem {AccountId = 1, Name = "Ali", Surname = "Uzun", Age = 20 },
+                new BankSystem {AccountId = 2, Name = "Rabia", Surname = "Beyaz", Age = 20 },
+                    new BankSystem {AccountId = 3, Name = "Fatih", Surname = "Kaya", Age = 20 },
+                        new BankSystem {AccountId = 4, Name = "Ayşe", Surname = "Kısa", Age = 20 },
+        };
+        [HttpGet]
+        public IEnumerable<BankSystem> Get()
+        {
+            return listofBankSystems;
+        }
+        [HttpPost]
+        public void post([FromBody]BankSystem bankSystem)
+        {
+            listofBankSystems.Add(bankSystem);
+        }
+        [HttpPut("{AccountId}")]
+        public void Put(int id, BankSystem banksystem)
+        {
+            listofBankSystems[id] = banksystem;
+        }
+        [HttpDelete("{AccountId}")]
+        public void delete(int id)
+        {
+            listofBankSystems.RemoveAt(id);
+        }
+
+    }
+}
---
+++ RESTful api in aspnet/Models/BankSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace RESTful_api_in_aspnet.Models
+{
+    public class BankSystem
+    {
+        public int AccountId { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
